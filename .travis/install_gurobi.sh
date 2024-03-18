61dd9aeef9a75c390ae6667829549111d6d53006
Joseph Cumines <joeycumines@gmail.com>
2024-03-18 21:11:41 +1000

    Delete travis ci cruft

--- .travis/install_gurobi.sh
+++
@@ -1,9 +0,0 @@
-cd $HOME
-
-wget http://packages.gurobi.com/7.5/gurobi7.5.2_linux64.tar.gz
-tar -xzf gurobi7.5.2_linux64.tar.gz
-
-export GUROBI_HOME=$HOME/gurobi752/linux64
-export GUROBI_LIB_NAME=gurobi75
-sudo ln -sf $GUROBI_HOME/lib/libgurobi75.so /usr/lib/libgurobi75.so
-cd -
