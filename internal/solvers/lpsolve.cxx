d486e1e2344a1b9753b715f4e37661d4bcfff1de
Joseph Cumines <joeycumines@gmail.com>
2024-03-20 21:06:00 +1000

    Fix passing constraints in the LPSolve solver

--- internal/solvers/lpsolve.cxx
+++ internal/solvers/lpsolve.cxx
@@ -1,6 +1,7 @@
 #include "lp_lib.h"
 #include "lpsolve.hpp"
 #include <iostream>
+#include <map>
 
 using namespace std;
 
@@ -54,43 +55,48 @@ void LPSolveSolver::addVars(int count, double *lb, double *ub, char *types)
 }
 
 void LPSolveSolver::addConstr(
-    int lhs_count, double *lhs_coeffs, uint64 *lhs_var_ids,
-    double lhs_constant,
-    int rhs_count, double *rhs_coeffs, uint64 *rhs_var_ids,
-    double rhs_constant, char sense)
+    int lhs_count, double* lhs_coeffs, uint64* lhs_var_ids, double lhs_constant,
+    int rhs_count, double* rhs_coeffs, uint64* rhs_var_ids, double rhs_constant, char sense)
 {
-    int var_count = lhs_count + rhs_count;
+    std::map<int, double> net_coeffs;
+
+    // Process LHS variables
+    for (int i = 0; i < lhs_count; ++i) {
+        int var_id = (int)lhs_var_ids[i] + 1; // Adjusting for 1-based indexing in LPSolve
+        net_coeffs[var_id] += lhs_coeffs[i];
+    }
+
+    // Process RHS variables, subtracting their coefficients
+    for (int i = 0; i < rhs_count; ++i) {
+        int var_id = (int)rhs_var_ids[i] + 1; // Adjusting for 1-based indexing in LPSolve
+        net_coeffs[var_id] -= rhs_coeffs[i];
+    }
+
+    // Prepare the arrays for add_constraintex call
+    int var_count = net_coeffs.size();
     REAL sparse_row[var_count];
     int colno[var_count];
+    int index = 0;
 
-    for (size_t i = 0; i < lhs_count; i++)
-    {
-        sparse_row[i] = lhs_coeffs[i];
-        colno[i] = (int) lhs_var_ids[i] + 1;
+    for (const auto& kv : net_coeffs) {
+        colno[index] = kv.first;
+        sparse_row[index] = kv.second;
+        ++index;
     }
 
-    for (size_t i = 0; i < rhs_count; i++)
-    {
-        sparse_row[lhs_count + i] = -rhs_coeffs[i];
-        colno[lhs_count + i] = (int) rhs_var_ids[i] + 1;
-    }
-
+    // Determine constraint type
     int constr_type;
-
-    switch (sense)
-    {
-        case '=':
-            constr_type = EQ;
-            break;
-        case '<':
-            constr_type = LE;
-            break;
-        case '>':
-            constr_type = GE;
-            break;
+    switch (sense) {
+        case '=': constr_type = EQ; break;
+        case '<': constr_type = LE; break;
+        case '>': constr_type = GE; break;
+        default: throw std::invalid_argument("Unsupported constraint sense.");
     }
 
+    // Calculate the constant part of the constraint
     REAL constant = rhs_constant - lhs_constant;
+
+    // Add the constraint to the model
     add_constraintex(lp, var_count, sparse_row, colno, constr_type, constant);
 }
 
