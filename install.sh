1a4f832c5b250fe4840d1b01b1c00a7e9d71da9d
Joseph Cumines <joeycumines@gmail.com>
2024-03-21 16:07:14 +1000

    Add swig to install script

--- install.sh
+++ install.sh
@@ -3,8 +3,7 @@
 set -xe
 
 if [[ $(uname) == 'Linux' ]]; then
-    sudo apt install -y libpcre3 libpcre3-dev autotools-dev byacc \
-        flex cmake build-essential autoconf lp-solve liblpsolve55-dev
+    sudo apt install -y libpcre3 libpcre3-dev autotools-dev byacc flex cmake build-essential autoconf lp-solve liblpsolve55-dev swig
 elif [[ $(uname) == 'Darwin' ]]; then
     brew install pcre autoconf
     brew install swig
