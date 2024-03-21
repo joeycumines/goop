ea655d2c2116436a443cc5bbe79a5fafe5891616
Joseph Cumines <joeycumines@gmail.com>
2024-03-21 14:21:00 +1000

    Remove project-relative include and lib strategy, add TODO

--- internal/solvers/lib.go
+++ internal/solvers/lib.go
@@ -1,5 +1,7 @@
 package solvers
 
-// #cgo CXXFLAGS: --std=c++11 -I../../include -I/usr/include/lpsolve
-// #cgo LDFLAGS: -L../../lib -L/usr/lib/lp_solve -L/usr/lib64 -llpsolve55
+// #cgo CXXFLAGS: --std=c++11 -I/usr/include/lpsolve -I/opt/homebrew/opt/lp_solve/include
+// #cgo LDFLAGS: -llpsolve55 -L/usr/lib/lp_solve -L/opt/homebrew/opt/lp_solve/lib -L/usr/lib64
 import "C"
+
+// TODO: Add support for replacing the paths above and, instead using a single path each, specified by env var, behind a build tag.
