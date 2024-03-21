4e34264064095873abd4b8181e8837f5f1b77c3e
Joseph Cumines <joeycumines@gmail.com>
2024-03-22 01:00:54 +1000

    Tidy up install script

--- install.sh
+++ install.sh
@@ -3,9 +3,7 @@
 set -xe
 
 if [[ $(uname) == 'Linux' ]]; then
-    sudo apt install -y libpcre3 libpcre3-dev autotools-dev byacc flex cmake build-essential autoconf lp-solve liblpsolve55-dev swig
+  sudo apt install libpcre3 libpcre3-dev autotools-dev byacc flex cmake build-essential autoconf lp-solve liblpsolve55-dev swig
 elif [[ $(uname) == 'Darwin' ]]; then
-    brew install pcre autoconf
-    brew install swig
-    brew install lp_solve
+  brew install pcre autoconf swig lp_solve
 fi
