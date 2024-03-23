6f8a3f580d0107f293abadcba53ab47062c47e60
Joseph Cumines <joeycumines@gmail.com>
2024-03-23 14:29:41 +1000

    Rejig search paths again, prefer /usr/local, deprioritise the system paths

--- internal/solvers/lib.go
+++ internal/solvers/lib.go
@@ -1,7 +1,7 @@
 package solvers
 
-// #cgo CXXFLAGS: --std=c++11 -I/usr/include/lpsolve -I/opt/homebrew/opt/lp_solve/include
-// #cgo LDFLAGS: -llpsolve55 -L/usr/lib -L/opt/homebrew/opt/lp_solve/lib -L/usr/lib64
+// #cgo CXXFLAGS: --std=c++11 -I/usr/local/include/lpsolve -I/opt/homebrew/opt/lp_solve/include -I/usr/include/lpsolve
+// #cgo LDFLAGS: -llpsolve55 -L/usr/local/lib -L/opt/homebrew/opt/lp_solve/lib -L/usr/lib -L/usr/lib64
 import "C"
 
 // TODO: Add support for replacing the paths above and, instead using a single path each, specified by env var, behind a build tag.
