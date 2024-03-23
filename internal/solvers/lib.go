1c2e2292d93d8044a03071dd85044b4a70d4fecf
Joseph Cumines <joeycumines@gmail.com>
2024-03-23 14:24:25 +1000

    Adjust search path to remove special ubuntu 20.04 case

--- internal/solvers/lib.go
+++ internal/solvers/lib.go
@@ -1,7 +1,7 @@
 package solvers
 
 // #cgo CXXFLAGS: --std=c++11 -I/usr/include/lpsolve -I/opt/homebrew/opt/lp_solve/include
-// #cgo LDFLAGS: -llpsolve55 -L/usr/lib/lp_solve -L/opt/homebrew/opt/lp_solve/lib -L/usr/lib64
+// #cgo LDFLAGS: -llpsolve55 -L/usr/lib -L/opt/homebrew/opt/lp_solve/lib -L/usr/lib64
 import "C"
 
 // TODO: Add support for replacing the paths above and, instead using a single path each, specified by env var, behind a build tag.
